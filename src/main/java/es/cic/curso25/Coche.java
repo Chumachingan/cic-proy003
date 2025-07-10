a3fc5ca2f59e662d5400a4bf07ef80fbcb7c8a8a
IvanCarrera <ivanvilac@gmail.com>
2025-07-10 11:21:03 +0200

    Test realizados Falta el coche

---
+++ src/main/java/es/cic/curso25/Coche.java
@@ -0,0 +1,28 @@
+package es.cic.curso25;
+
+public class Coche {
+
+    public int getVelocidad() {
+        // TODO Pendiente de Completar
+        throw new UnsupportedOperationException("No implementado");
+    }
+
+    public double getConsumo() {
+        // TODO Pendiente de Completar
+        throw new UnsupportedOperationException("No implementado");
+    }
+
+    public int acelerar(int incrementoDeVelocidad) {
+
+        // TODO Pendiente de Completar
+        throw new UnsupportedOperationException("No implementado");
+
+    }
+
+    public int frenar(int decrementoDeVelocidad) {
+
+        // TODO Pendiente de Completar
+        throw new UnsupportedOperationException("No implementado");
+
+    }
+}
---
+++ src/test/java/es/cic/curso25/CocheTest.java
@@ -0,0 +1,53 @@
+package es.cic.curso25;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class CocheTest {
+
+    private Coche cut;
+
+    @BeforeEach
+    private void setUp() {
+        cut = new Coche();
+    }
+
+    @Disabled
+    @Test
+    void testAcelerar() {
+        cut.acelerar(34);
+        int velocidadFinal = cut.acelerar(23);
+
+        assertEquals(57, velocidadFinal);
+    }
+
+    @Disabled
+    @Test
+    void testFrenar() {
+        cut.frenar(50);
+        int velocidadFinal = cut.frenar(23);
+
+        assertEquals(27, velocidadFinal);
+    }
+
+    @Disabled
+    @Test
+    void testGetConsumo() {
+        cut.acelerar(50);
+        double consumo = cut.getConsumo();
+
+        assertEquals(2.15, consumo);
+    }
+
+    @Disabled
+    @Test
+    void testGetVelocidad() {
+        cut.acelerar(50);
+        int velocidad = cut.getVelocidad();
+
+        assertEquals(50, velocidad);
+    }
+}
